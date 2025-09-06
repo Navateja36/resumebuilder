333c65d0b5190ca9e350413bf9884a1c3944afba
Navateja36 <21211a05g6@bvrit.ac.in>
2025-09-06 18:48:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
     // this code is for people who wants to select the text from the pdf 
-    import React, { useState, useRef, forwardRef } from 'react';
+    import React, { useState,  forwardRef } from 'react';
     import { Page, Text, View, Document, StyleSheet, Font, PDFDownloadLink } from '@react-pdf/renderer';
     import './App.css';
     import { Analytics } from '@vercel/analytics/react';
